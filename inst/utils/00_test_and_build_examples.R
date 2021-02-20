adea42d7cd58a98f35b6af7abd2e2adb3ceb9194
Felipe Campelo <fcampelo@gmail.com>
2021-02-20 20:31:06 +0000

    update examples

--- inst/utils/00_test_and_build_examples.R
+++ inst/utils/00_test_and_build_examples.R
@@ -1,7 +1,7 @@
 inputfiles <- dir("./data_files/parameters_validation", full.names = TRUE)
 
 # run for all specs files
-for (i in seq_along(inputfiles)){
+for (i in 4:9){#seq_along(inputfiles)){
   cat("\nProcessing example", i, "of", length(inputfiles), "\n")
   defs <- run_CALANGO(inputfiles[i], cores = parallel::detectCores() - 1)
 }
