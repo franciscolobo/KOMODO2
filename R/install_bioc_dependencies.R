77f32148ebe1372b92df631635c59dc89bcd8031
fcampelo <fcampelo@gmail.com>
2021-07-03 12:36:19 +0100

    Updated install_bioc_dependencies()

--- R/install_bioc_dependencies.R
+++ R/install_bioc_dependencies.R
@@ -14,7 +14,8 @@
 #' packages. Further arguments to this function are passed as a list.
 #'
 #' @param bioc.args list containing further arguments to be passed
-#' down to `BiocManager::install()`.
+#' down to `BiocManager::install()`. 
+#' @param force logical: reinstall already-installed packages?
 #'
 #' @export
 #'
@@ -25,17 +26,36 @@
 #' 
 #' @return No return value, called for side effects.
 
-install_bioc_dependencies <- function(bioc.args = list()){
+install_bioc_dependencies <- function(bioc.args = list(), 
+                                      force = FALSE){
   # ================== Sanity checks ==================
-  assertthat::assert_that(is.list(bioc.args))
+  assertthat::assert_that(is.list(bioc.args),
+                          is.logical(force), length(force) == 1)
   
-  # Install the required Bioconductor packages
-  message("\nInstalling AnnotationDbi, KEGGREST and GO.db from BioConductor ", 
-          BiocManager::version(), " (if needed)")
-  bioc.args$pkgs <- c("AnnotationDbi",
-                      "KEGGREST",
-                      "GO.db")
-  bioc.args$ask = TRUE
+  pkgs <- c("AnnotationDbi", "KEGGREST", "GO.db")
+  makeInst <- FALSE
   
-  do.call(BiocManager::install, bioc.args)
+  if (force){
+    makeInst <- TRUE
+    bioc.args$force <- TRUE
+  } else {
+    x <- rownames(utils::installed.packages())
+    idx  <- which(!toCheck %in% x)
+    if (length(idx) > 0){
+      pkgs <- pkgs[idx]
+      makeInst <- TRUE
+    }
+  }
+  
+  if (makeInst){
+    message(paste0("\nInstalling package(s): ",
+                   paste(pkgs, collapse = ", "),
+                   "\nfrom BioConductor version ", 
+                   BiocManager::version()))
+    bioc.args$pkgs <- pkgs
+    bioc.args$ask  <- TRUE
+    
+    do.call(BiocManager::install, bioc.args)
+  }
+  invisible(TRUE)
 }
