6e408533e0b8275b5aa1cc4bb7787bda509b7679
fcampelo <fcampelo@gmail.com>
2021-08-26 11:54:06 +0100

    Update 00_test_and_build_examples.R

--- inst/utils/00_test_and_build_examples.R
+++ inst/utils/00_test_and_build_examples.R
@@ -1,8 +1,8 @@
-retrieve_data_files(target.dir = "data")
+# retrieve_data_files(target.dir = "data") # uncomment if needed
 
 inputfiles <- dir("data/parameters/", full.names = TRUE)
 
-ncores = 7
+ncores <- 2 # parallel::detectCores() - 1
 
 # run for all specs files
 for (i in seq_along(inputfiles)){
